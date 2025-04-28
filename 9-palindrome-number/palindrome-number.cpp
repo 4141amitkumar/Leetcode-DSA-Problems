597e4fccef9d3101480d078e52d2bef8ae88c564
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-04-28 18:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (9.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,10 +3,9 @@ public:
     bool isPalindrome(int x) {
         if (x<0) return false;
         string s = to_string(x);
-        int n = s.size();
-        for(int i=0;i<n/2;i++){
-            if(s[i]!=s[n-i-1]) return false;
-        }
-        return true;
+        string k = s;
+        reverse(k.begin(),k.end());
+        if(s==k) return true;
+        return false;
     }
 };

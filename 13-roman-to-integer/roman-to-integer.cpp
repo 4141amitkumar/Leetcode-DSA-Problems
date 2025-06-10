288a705c38c36f5607eac6734c250623a71e1a26
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-06-10 20:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (52.92%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.size();
-        unordered_map<char,int> m;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='I' && (m['V']!=0 || m['X']!=0)) m['I']--;
-            else if(s[i]=='X' && (m['L']!=0 || m['C']!=0)) m['X']--;
-            else if(s[i]=='C' && (m['D']!=0 || m['M']!=0)) m['C']--;
-            else m[s[i]]++;
+        int total=0;
+        unordered_map<char,int> m={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int prev=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(prev>m[s[i]]) total-=m[s[i]];
+            else total+=m[s[i]];
+            prev=m[s[i]];
         }
-        return m['M']*1000+m['D']*500+m['C']*100+m['L']*50+m['X']*10+m['V']*5+m['I'];
+        return total;
     }
 };

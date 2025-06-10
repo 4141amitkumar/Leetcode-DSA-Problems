d027c05e230d65d95f399a76d8ade5369471c486
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-06-10 17:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (27.18%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -5,14 +5,12 @@ public:
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
         }
-        vector<int> v1;
-        vector<int> v2;
+        int maxodd = INT_MIN;
+        int mineven = INT_MAX;
         for(auto x:m){
-            if(x.second%2!=0)v1.push_back(x.second);
-            if(x.second%2==0)v2.push_back(x.second);
+            if(x.second%2!=0)maxodd=max(maxodd,x.second);
+            if(x.second%2==0)mineven=min(mineven,x.second);
         }
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-        return v1[v1.size()-1]-v2[0];
+        return maxodd-mineven;
     }
 };

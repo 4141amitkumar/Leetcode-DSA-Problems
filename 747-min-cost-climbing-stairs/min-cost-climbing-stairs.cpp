7ac04ccc1f2012cd9e129825c26df9bb65da067b
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-07-02 08:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (73.72%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    // DP approach solution
-    int helper(vector<int>& cost, int i, vector<int>& dp){
-        if(i>=cost.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = cost[i] + min(helper(cost,i+1,dp), helper(cost,i+2,dp));
-    }
+    //Tabulation method (Bottom up approach)
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(),-1);
-        return min(helper(cost,0,dp),helper(cost,1,dp));
+        int n = cost.size();
+        for(int i=2;i<n;i++){
+            cost[i] += min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[n-1],cost[n-2]);
     }
 };

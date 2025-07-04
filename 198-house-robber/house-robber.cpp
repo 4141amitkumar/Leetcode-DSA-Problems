9207a6cf78784215089c323d5c7a3cd7cb5613f4
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-07-04 21:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    // dp (top-down approach) recursion + memoization
+    int helper(int i, vector<int>& nums, vector<int>& dp){
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+
+        int rob = nums[i] + helper(i-2,nums,dp);
+        int skip = helper(i-1,nums,dp);
+
+        return dp[i]=max(rob,skip);
+    }
     int rob(vector<int>& nums) {
-        //Tabulation approach (bottom-up)
         int n = nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
         vector<int> dp(n,-1);
-        dp[0]=nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        }
-        return dp[n-1];
+        return helper(n-1,nums,dp);
     }
 };

e3d444ccb48da437200b18ea4a0dca7e4bdede9f
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-08-13 17:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,7 +3,6 @@ public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
         double x = (log(n))/log(3);
-        if(floor(x)==x) return true;
         return abs(x-round(x))<1e-10;
     }
 };

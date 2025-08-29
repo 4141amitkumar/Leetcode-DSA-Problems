984bf7f3387ef7401eccc089f771d81be4592511
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-08-29 11:38:42 +0530

    Time: 1 ms (4.98%) | Memory: 8.4 MB (18.51%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool checkString(string s) {
         int i=0;
-        bool flag=false;
         while(i<s.size()){
-            if(s[i]=='b') flag=true;
-            else if(flag==true && s[i]=='a') return false;
+            if(s[i]=='b' && s[i+1]=='a') return false;
             i++;
         }
         return true;

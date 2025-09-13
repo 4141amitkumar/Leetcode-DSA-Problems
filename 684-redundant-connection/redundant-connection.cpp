addb868538176ce7efc917a0ab8d88ba6547c9cf
4141amitkumar <136194744+4141amitkumar@users.noreply.github.com>
2025-09-13 23:37:59 +0530

    Time: 15 ms (9.19%) | Memory: 37.4 MB (5.21%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,41 +1,27 @@
-class DSU{
-    vector<int> parent, rank;
-public:
-    DSU(int n){
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        for(int i=0;i<n;i++) parent[i]=i;
-    }
-    int find( int x){
-        if(parent[x]!=x){
-            parent[x]=find(parent[x]);
-        }
-        return parent[x];
-    }
-    bool unite(int x,int y){
-        int px = find(x);
-        int py = find(y);
-        if(px==py) return false;
-        if(rank[px]<rank[py]) parent[px]=py;
-        else if(rank[px]>rank[py]) parent[py]=px;
-        else {
-            parent[py]=px;
-            rank[px]++;
-        }
-        return true;
-    }
-};
 class Solution {
 public:
+    bool dfs(int src, int target, vector<vector<int>>& graph, vector<int>& vis){
+        if(src==target) return true;
+        vis[src]=1;
+        for(int nbr:graph[src]){
+            if(!vis[nbr]){
+                if(dfs(nbr,target,graph,vis)) return true;
+            }
+        }
+        return false;
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        DSU dsu(n);
+        vector<vector<int>> graph(n+1); //1-indexed
         for(auto &edge: edges){
             int u=edge[0], v=edge[1];
-            if(!dsu.unite(u,v)){
-                //cycle detected
-                return edge;
+            vector<int> vis(n+1,0);
+            if(!graph[u].empty() && !graph[v].empty()){
+                if(dfs(u,v,graph,vis)) return edge;
             }
+            //add edge in graph
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
         return {};
     }
